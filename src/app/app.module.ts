70b4c1591becc50860feb5c78fa4500df3e803ec
Melissa Seren <mee_seren@outlook.com>
2022-03-25 19:53:58 -0300

    add module photos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { PhotoComponent } from './photo/photo.component';
+import { PhotoComponent } from './photos/photo/photo.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/photos/photos.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PhotoComponent } from './photo/photo.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [ PhotoComponent ],
+  exports: [ PhotoComponent ]
+})
+export class PhotosModule { }
